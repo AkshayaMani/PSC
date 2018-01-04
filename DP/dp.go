d20cfa917747a2afd736a4ff0c146deac0f713cf
AkshayaMani <am3227@georgetown.edu>
2018-01-03 20:30:31 -0500

    Connect to Tor control port

--- DP/dp.go
+++ DP/dp.go
@@ -12,18 +12,29 @@ import (
     "encoding/binary"
     "flag"
     "fmt"
+    "github.com/armon/go-radix"
     "github.com/dedis/crypto/abstract"
     "github.com/dedis/crypto/nist"
     "github.com/golang/protobuf/proto"
+    "golang.org/x/net/publicsuffix"
+    "hash/fnv"
     "io"
     "io/ioutil"
+    "math"
     "net"
     "os"
     "PSC/DP/dpres"
+    "PSC/goControlTor"
+    "PSC/match"
     "PSC/TS/tsmsg"
+    "strings"
+    "strconv"
     "sync"
     "syscall"
+    "time"
 )
+
+const p = 200003 //Universal hash parameter p
 
 var no_CPs int32 //No.of CPs
 var no_DPs int32 //No. of DPs
@@ -32,6 +43,7 @@ var b int64 //Hash table size
 
 var ts_hname = "TS" //TS hostname
 var ts_ip = "10.176.5.15" //TS IP
+var query string //Query
 var cp_hname []string //CP hostnames
 var dp_hname []string //DP hostnames
 var cp_ips []string //CP IPs
@@ -49,9 +61,20 @@ var c []abstract.Scalar //Ciphers
 var cs [][]abstract.Scalar //Cipher share
 var wg = &sync.WaitGroup{} //WaitGroup to wait for all goroutines to shutdown
 
+var torControl = &goControlTor.TorControl{} //Tor control port connection
+var domain_radix = new(radix.Tree) //Suffix match tree
+var message chan string //Channel to receive message from Tor control port
+var q_to_e = map[string]string{ //Map query
+
+    "ExitFirstLevelDomainWebInitialStream": "PRIVCOUNT_STREAM_ENDED",
+    "ExitDomainSuffixMatchWebInitialStream": "PRIVCOUNT_STREAM_ENDED",
+}
+
 func main() {
 
-    dp_ip := parseCommandline(os.Args) //Parse DP common name
+    dp_ip, control_addr, control_port, passwd_file := parseCommandline(os.Args) //Parse DP common name & IP, Tor control address & port no., and hashed password file path
+
+    torControlPortConnect(control_addr, control_port, passwd_file)
 
     for{
         //Initialize global variables
@@ -65,6 +88,10 @@ func main() {
         wg.Add(1) //Increment WaitGroup counter
 
         go acceptConnections() //Accept connections
+
+        wg.Add(1) //Increment WaitGroup counter
+
+        go torControlPortReceive(torControl) //Receive events from Tor control port
 
         loop:
 
@@ -80,16 +107,31 @@ func main() {
                     if ts_hname == com_name {//If data received from TS
 
                         //Handle TS connection
-                        handleClients(conn, com_name)
+                        handleTS(conn, com_name)
 
                     } else { //If not TS
 
                         conn.Close() //Close connection
                     }
 
+                case msg := <- message:
+
+                    event, _, err1 := torControl.CommandParse(msg) //Print command
+                    checkError(err1)
+
+                    if len(event) != 0 {
+
+                        if event[0] == q_to_e[query] {
+
+                            handle_stream_event(event[1:], query)
+                        }
+                    }
+
                 case <-finish:
 
-                    wg.Wait()
+                    close(message) //Close Tor control port message channel
+
+                    wg.Wait() //Wait for all go routines to finish
 
                     if step_no == 4 { //Finish
 
@@ -130,10 +172,12 @@ func sendTSSignal(sno uint32) {
 
 //Input: Client Socket Channel, Client common name
 //Function: Handle client connection
-func handleClients(conn net.Conn, com_name string) {
+func handleTS(conn net.Conn, com_name string) {
 
     //Receive Data
     buf := receiveData(conn)
+
+    conn.Close() //Close connection
 
     if f_flag == false { //Finish flag not set
 
@@ -147,6 +191,13 @@ func handleClients(conn net.Conn, com_name string) {
                 proto.Unmarshal(buf, config) //Parse TS configuration
 
                 assignConfig(config) //Assign configuration
+
+                if query == "ExitDomainSuffixMatchWebInitialStream" {
+
+                    domain_list := match.LoadDomainList("domain-top-fld-1m.txt")
+
+                    domain_radix = match.SuffixMatchCreateRadix(domain_list, ".")
+                }
 
                 fmt.Println("Sending TS signal. Step No.", step_no)
                 sendTSSignal(ts_s_no+step_no) //Send signal to TS
@@ -173,6 +224,7 @@ func handleClients(conn net.Conn, com_name string) {
 
                 if *sig.Fflag == true { //If finish flag set
 
+                    fmt.Println("TS sent finish")
                     shutdownDP() //Shutdown DP gracefully
 
                 } else { //Finish flag not set
@@ -216,6 +268,7 @@ func handleClients(conn net.Conn, com_name string) {
                         } else if step_no == 2 { //If step no. 2
 
                             fmt.Println("Started data collection")
+
                             collectData() //Start collecting data
 
                         } else if step_no == 3 { //If step no. 3
@@ -279,21 +332,153 @@ func handleClients(conn net.Conn, com_name string) {
     }
 }
 
+//Input: Tor control address, Tor control port and Tor control hashed password file path
+//Function: Authenticate and establish connection to Tor control port
+func torControlPortConnect(control_addr, control_port, passwd_file string) {
+
+    //Dial Tor control port
+    err := torControl.Dial("tcp", control_addr + ":" + control_port, passwd_file)
+    checkError(err)
+
+    err = torControl.SendCommand("PROTOCOLINFO 1\r\n")
+    checkError(err)
+
+    for {
+        msg, err := torControl.ReceiveCommand() //Receive command
+        checkError(err)
+
+        _, state, err1 := torControl.CommandParse(msg) //Print command
+        checkError(err1)
+
+        if state == "waiting" {
+
+            break
+        }
+    }
+}
+
 //Function: Collect data from Tor using oblivious counters
 func collectData () {
 
-    //c[event].Add(c[event], suite.Scalar().Pick(rand)) //Increment counter by adding a random number
+    err := torControl.StartCollection(q_to_e[query])
+    checkError(err)
+    time.Sleep(24 * time.Duration(epoch) * time.Hour)
+    torControl.StopCollection()
+}
+
+//Input: Tor control port connection
+//Function: Receive events from Tor control port
+func torControlPortReceive(torControl *goControlTor.TorControl) {
+
+    defer wg.Done() //Decrement counter when goroutine completes
+
+    for {
+
+	select {
+
+            case _, ok := <- message:
+
+                if !ok {
+
+                    return
+
+                } else {
+
+                    msg, err := torControl.ReceiveCommand() //Receive command
+                    checkError(err)
+
+                    message <- msg
+                }
+
+            default:
+        }
+    }
+}
+
+//Input: Event, Query
+//Function: Handle stream event and increment counter
+func handle_stream_event(event []string, query string) {
+
+    port := event[3] //Remote port
+    remote_host := event[8] //Remote host address
+    //remote_ip := event[9] //Resolved remote IP address
+    exit_stream_number := event[10] //Circuit exit stream number
+
+    stream_circ := "Subsequent" //Set stream as subsequent stream
+
+    if exit_stream_number == strconv.Itoa(1) {
+
+        stream_circ = "Initial" //First stream on circuit
+    }
+
+    stream_web := "NonWeb" //Set stream as non-web
+
+    if port == strconv.Itoa(80) || port == strconv.Itoa(443) {
+
+        stream_web = "Web" //Web stream
+    }
+
+    host_ip_version := "Hostname" //Set as hostname
+
+    if net.ParseIP(remote_host) != nil {
+
+        if net.ParseIP(remote_host).To4 != nil {
+
+            host_ip_version = "IPv4Literal" //IPv4 address
+
+        } else {
+
+            host_ip_version = "IPv6Literal" //IPv6 address
+        }
+    }
+
+    if host_ip_version == "Hostname" && stream_web == "Web" && stream_circ == "Initial" {
+
+        if query == "ExitFirstLevelDomainWebInitialStream" {
+
+           if fld, _ := publicsuffix.EffectiveTLDPlusOne(remote_host); fld != "" {
+
+               incrementCounter(fld) //Increment counter
+           }
+
+        } else if query == "ExitDomainSuffixMatchWebInitialStream" {
+
+            if suffix_match := match.SuffixMatch(domain_radix, remote_host, "."); suffix_match != "" {
+
+                incrementCounter(suffix_match) //Increment counter
+            }
+        }
+    }
+}
+
+//Input: Event
+//Function: Hash and increment counter
+func incrementCounter(event string) {
+
+    suite := nist.NewAES128SHA256P256()
+    rand := suite.Cipher(abstract.RandomKey)
+
+    h := fnv.New32a()
+    h.Write([]byte(strings.ToLower(event)))
+    key := math.Mod(math.Mod(((4.0*float64(h.Sum32()))+7.0), float64(p)), float64(b)) //Map to one of the counters
+    c[int(key)].Add(c[int(key)], suite.Scalar().Pick(rand)) //Increment counter by adding a random number
 }
 
 //Input: Command-line Arguments
 //Function: Parse Command-line Arguments
-func parseCommandline(arg []string) (string){
+func parseCommandline(arg []string) (string, string, string, string) {
 
     var dp_ip string //DP IP
     var e_flag = false //Exit flag
+    var control_addr string //Tor control address
+    var control_port string //Tor control port
+    var passwd_file string //Tor control hashed password file path
 
     flag.StringVar(&dp_cname, "d", "", "DP common name (required)")
     flag.StringVar(&dp_ip, "i", "", "DP IP (required)")
+    flag.StringVar(&control_addr, "ca", "127.0.0.1", "Tor control port listen address")
+    flag.StringVar(&control_port, "cp", "9051", "Tor control port")
+    flag.StringVar(&passwd_file, "pf", "control_password.txt", "Tor control hashed password file path")
     flag.Parse()
 
     if dp_cname == "" || dp_ip == "" {
@@ -319,7 +504,7 @@ func parseCommandline(arg []string) (string){
         os.Exit(0) //Exit
     }
 
-    return dp_ip
+    return dp_ip, control_addr, control_port, passwd_file
 }
 
 //Function: Initialize variables
@@ -340,6 +525,7 @@ func initValues() {
     ts_config_flag = true //TS configuration flag
     ln = nil //Server listener
     finish = make(chan bool) //Channel to send finish flag
+    message = make(chan  string) //Channel to receive message from Tor control port
     clients = make(chan net.Conn) //Channel to handle simultaneous client connections
     k = nil //CP-DP Keys
     c = nil //Ciphers
@@ -354,6 +540,7 @@ func assignConfig(config *TSmsg.Config) {
     ts_s_no = uint32(*config.SNo) //TS session no.
     epoch = int(*config.Epoch) //Epoch
     no_CPs = *config.Ncps //No. of CPs
+    query = *config.Query //Query
     cp_hname = make([]string, no_CPs) //CP hostnames
     cp_ips = make([]string, no_CPs) //CP IPs
 
