8e30ea6e280691575ef90cd34d8607ca711cae26
teor <teor2345@gmail.com>
2018-02-13 17:48:19 +1100

    Always use CA directory to write pem files
    This avoids having to edit openssl.cnf for every new CP/DP

--- keypair.sh
+++ keypair.sh
@@ -2,6 +2,6 @@ openssl req -newkey rsa:4096 -keyout $2.key -out $2.csr -config openssl.cnf -day
 openssl rsa -in $2.key -out $2.key.insecure
 mv $2.key.insecure $2.key
 openssl ca -in $2.csr -config openssl.cnf
-mv $1/certs/*.pem $1/certs/$2.cert
+mv CA/certs/*.pem $1/certs/$2.cert
 mv $2.csr $1/csr/
 mv $2.key $1/private/
