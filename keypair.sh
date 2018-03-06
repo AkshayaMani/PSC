c1e7555b83cff75087915ad871a487317b1dc2e6
Matt Traudt <pastly@users.noreply.github.com>
2018-03-06 15:03:00 -0500

    Don't encrypt the key --> don't have to decrypt it

--- keypair.sh
+++ keypair.sh
@@ -1,6 +1,4 @@
-openssl req -newkey rsa:4096 -keyout $2.key -out $2.csr -config openssl.cnf -days 3650
-openssl rsa -in $2.key -out $2.key.insecure
-mv $2.key.insecure $2.key
+openssl req -newkey rsa:4096 -nodes -keyout $2.key -out $2.csr -config openssl.cnf -days 3650
 openssl ca -in $2.csr -config openssl.cnf
 mv CA/certs/*.pem $1/certs/$2.cert
 mv $2.csr $1/csr/
