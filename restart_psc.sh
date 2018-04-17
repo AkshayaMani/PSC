c79fefe29d20bbaba4eee4cfa0e0312056f0a567
AkshayaMani <AkshayaMani@users.noreply.github.com>
2018-04-16 19:10:12 -0700

    Update restart_psc.sh
    Can we rename it to update_psc.sh? Running this script will kill current running versions and update. PSC will automatically restart with the latest version.

--- restart_psc.sh
+++ restart_psc.sh
@@ -8,3 +8,24 @@ killall go
 killall ts
 killall cp
 killall dp
+
+# Argument processing
+if [ $# -lt 1 ]; then
+    echo "usage: $0 GoEnv"
+    exit 1
+fi
+
+PSC_GOENV="$1"
+
+# Prepare to launch the command
+[ -f "$HOME/.envirius/nv" ] && . ~/.envirius/nv
+
+# Activate environment
+nv on "$PSC_GOENV"
+
+# Upgrade PSC
+cd $GOPATH/src/PSC
+git pull
+
+# Deactivate environment
+nv off
